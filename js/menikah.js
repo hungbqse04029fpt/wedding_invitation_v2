43493bbfe1d566f87e3f18f5c3f9b1df5ab174d6
DESKTOP-LC7IUSJ\quang <quanghung445@gmail.com>
2025-01-01 22:41:59 +0900

    Initial commit

--- js/menikah.js
+++ js/menikah.js
@@ -35,23 +35,23 @@ $(document).on("click", 'a[href^="#"]', function(event) {
 
 // When the user scrolls down 20px from the top of the document, show the scroll up button
 window.onscroll = function() {
-  scrollFunction();
+  //scrollFunction();
 };
 
-function scrollFunction() {
+/*function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("toTop").style.display = "block";
   } else {
     document.getElementById("toTop").style.display = "none";
   }
-}
+}*/
 
 // Preloader
 $(document).ready(function($) {
   $(".preloader-wrapper").fadeOut();
   $("body").removeClass("preloader-site");
 });
-$(window).load(function() {
+/*$(window).load(function() {
   var Body = $("body");
   Body.addClass("preloader-site");
-});
+});*/
